ce1db5dec4f4da75b5b9efbfaf50f19690ce4df9
Hantao Zhong <moon991015@gmail.com>
2021-03-11 01:51:23 +0800

    Add cw1 part3

--- Coursework1/haskell-quoridor/src/Players/Reed.hs
+++ Coursework1/haskell-quoridor/src/Players/Reed.hs
@@ -11,14 +11,22 @@ module Players.Reed where
 import Types
 import Action
 import Game
-
+import Players.Minimax
 -- Create a player that starts with the Reed opening. After that, you may use your minimax action or
 -- the given action for DumbPlayer. 
 -- [Hint 1: Use the variable 'turn' in Player.]
 -- [Hint 2: Use 'wallTop' to get the walls you need.]
 -- [Hint 3: Don't forget to check that the action is valid using 'validWallAction'.]
 reedPlayerAction :: Board -> [Player] -> String -> Int -> Maybe Action
-reedPlayerAction = undefined
+reedPlayerAction b players s i
+    | turn (head players) == 1 = checkWall b players s i (wallTop('c',3))
+    | turn (head players) == 2 = checkWall b players s i (wallTop('f',3))
+    | otherwise = minimaxAction b players s i
+
+checkWall:: Board -> [Player] -> String -> Int -> Wall ->Maybe Action
+checkWall b ps s i w
+    | validWallAction (Game b ps) w = Just (Place w)
+    | otherwise = minimaxAction b ps s i
 
 -- We build a Reed player from a name, a starting cell, a number of walls, an array of winning
 -- positions and 'commandToAction'.
